a01c9c748eb660e5496681c5893339063c63a0f1
Dmitriy Angve <kirirch129@gmail.com>
2022-10-18 14:59:36 +0300

    #

--- script.js
+++ script.js
@@ -234,3 +234,24 @@ console.log(now - 1991 > now - 2018);
 // } else {
 //   console.log("Height is UNDEFINED");
 // }
+
+// const age = "18";
+// if (age === 18) console.log("You just became an adult (strict)");
+// if (age == 18) console.log("You just became an adult (loose)");
+
+// const favourite = Number(prompt("What's ypur favorite number?"));
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite === 23) {
+//   // 23 === 23
+//   console.log("Cool! 23 is amazing number!");
+// } else if (favourite === 7) {
+//   console.log("7 is also a cool number!");
+// } else if (favourite === 9) {
+//   console.log("9 is also a cool number!");
+// } else {
+//   console.log("Number is not 23 or 7 or 9");
+// }
+
+// if (favourite !== 23) console.log("Why not 23?");
