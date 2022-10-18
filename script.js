6568be25452e6c09033ee00c8351ffdaa07b584b
Dmitriy Angve <kirirch129@gmail.com>
2022-10-18 14:43:20 +0300

    #

--- script.js
+++ script.js
@@ -195,20 +195,42 @@ console.log(now - 1991 > now - 2018);
 // }
 
 // type conversion
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
-console.log(Number("Dmitriy"));
-console.log(typeof NaN);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+// console.log(Number("Dmitriy"));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-// type coercion
-console.log("I'am " + 23 + " years old");
-console.log("I'am " + String(23) + " years old");
-console.log("23" - "10" - 3);
-console.log("23" / "2");
+// // type coercion
+// console.log("I'am " + 23 + " years old");
+// console.log("I'am " + String(23) + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
 
-let n = "1" + 1; // '11'
-n = n - 1; // 11-1
-console.log(n);
+// let n = "1" + 1; // '11'
+// n = n - 1; // 11-1
+// console.log(n);
+
+// falsy values: 0, ''. undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Dmitriy"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 100;
+// if (money) {
+//   console.log("Don't spend it all ;)");
+// } else {
+//   console.log("You should get a job!");
+// }
+
+// let height = 0;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
