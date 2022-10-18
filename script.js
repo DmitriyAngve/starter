716cd7b1381d66b5117d21c4f1db696f30924b10
Dmitriy Angve <kirirch129@gmail.com>
2022-10-18 10:41:14 +0300

    5

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let job2 = "teacher";
 
 console.log(myFirstJob);
 */
-
+/*
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -45,3 +45,18 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Angve";
+console.log(lastName);
