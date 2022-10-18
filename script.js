177d6df51ce2152a99fd52ff3558a2f5a526cf55
Dmitriy Angve <kirirch129@gmail.com>
2022-10-18 14:28:22 +0300

    #

--- script.js
+++ script.js
@@ -193,3 +193,22 @@ console.log(now - 1991 > now - 2018);
 // } else {
 //   console.log(`John's (${BMIjohn}) BMI is higher than Mark's (${BMImark})!`);
 // }
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+console.log(Number("Dmitriy"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I'am " + 23 + " years old");
+console.log("I'am " + String(23) + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+
+let n = "1" + 1; // '11'
+n = n - 1; // 11-1
+console.log(n);
