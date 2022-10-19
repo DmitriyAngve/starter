87f7fa3acb7cbadd240870ca590ed4d64b3b78bb
Dmitriy Angve <kirirch129@gmail.com>
2022-10-19 11:51:16 +0300

    #

--- script.js
+++ script.js
@@ -416,3 +416,24 @@ console.log(now - 1991 > now - 2018);
 // console.log(drink2);
 
 // console.log(`I like to drink ${age >= 18 ? "beer" : "water"}`);
+
+// FIRST SOLUTION
+// const bill = 275;
+// bill >= 50 && bill <= 300
+//   ? console.log(
+//       `The bill was ${bill}, the tip was ${bill * 0.15}, and the total value ${
+//         bill + bill * 0.15
+//       }`
+//     )
+//   : console.log(
+//       `The bill was ${bill}, the tip was ${bill * 0.2}, and the total value ${
+//         bill + bill * 0.2
+//       }`
+//     );
+
+// SECOND SOLUTION
+// const bill = 40;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+// );
