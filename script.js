1ee87b4f98854ff44804b5c98ca15acc278abf65
Dmitriy Angve <kirirch129@gmail.com>
2022-10-19 11:12:26 +0300

    #

--- script.js
+++ script.js
@@ -358,27 +358,43 @@ console.log(now - 1991 > now - 2018);
 //     console.log("Its nothing");
 // }
 
-const day = "monday";
+// const day = "monday";
 
-switch (day) {
-  case "monday": // if day === 'monday'
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break;
-  case "tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-  case "thursday":
-    console.log("Write code examples");
-    break;
-  case "friday":
-    console.log("Recording videos");
-    break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend ;D");
-    break;
-  default:
-    console.log("Not a valid day!");
-}
+// switch (day) {
+//   case "monday": // if day === 'monday'
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break;
+//   case "tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Write code examples");
+//     break;
+//   case "friday":
+//     console.log("Recording videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjoy the weekend ;D");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+// }
+
+// const day = "monday";
+// if (day === "monday") {
+//   console.log("Plan course structure");
+//   console.log("Go to coding meetup");
+// } else if (day === "tuesday") {
+//   console.log("Prepare theory videos");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("Write code examples");
+// } else if (day === "friday") {
+//   console.log("Recording videos");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("Enjoy the weekend ;D");
+// } else {
+//   console.log("Not a valid day!");
+// }
